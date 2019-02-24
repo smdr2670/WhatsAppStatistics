2c2b7be081c42fe489354c06f8a317e1909f64e7
Daniel Rembold <daniel.rd@hotmail.de>
2019-02-24 02:12:07 +0100

    Added Emoji parsing, simplified getting cumulative stats

--- WhatsApp.py
+++ WhatsApp.py
@@ -1,9 +1,6 @@
 import re
-
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-
+import WhatsAppVisualizationModule as statPlot
 
 class Message(object):
     def __init__(self, date, sender, content):
@@ -45,70 +42,6 @@ class MessageList(object):
             self.appendTrailingMessage(msg)
 
 
-
-def plotTotalNumberOfMessages(msgList):
-    numberOfMessagesSentDict = {}
-    for sender in msgList.senderList:
-        numberOfMessagesSentDict[sender] = [0, 0]
-
-    for message in msgList.msgList:
-        if message.isMedia:
-            numberOfMessagesSentDict[message.sender][1] = numberOfMessagesSentDict[message.sender][1] + 1
-        else:
-            numberOfMessagesSentDict[message.sender][0] = numberOfMessagesSentDict[message.sender][0] + 1
-
-    for sender in numberOfMessagesSentDict:
-        print("{0} : {1} text messages, {2} media messages".format(
-            sender, numberOfMessagesSentDict[sender][0], numberOfMessagesSentDict[sender][1]))
-
-    listOfSenders = []
-    listOfTotalMessages = []
-    listOfMediaMessages = []
-
-    # labels for bars
-
-    for key, value in numberOfMessagesSentDict.items():
-        listOfSenders.append(key)
-        listOfTotalMessages.append(value[0])
-        listOfMediaMessages.append(value[1])
-
-    ind = np.arange(len(listOfSenders))
-
-    width = 0.35  # the width of the bars
-
-    _, ax = plt.subplots()
-    rects1 = ax.bar(ind - width/2, listOfTotalMessages, width,
-                    color='SkyBlue', label='Text messages')
-    rects2 = ax.bar(ind + width/2, listOfMediaMessages, width,
-                    color='IndianRed', label='Media Messages')
-
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Number of messages')
-    ax.set_title('Group chat ...')
-    ax.set_xticks(ind)
-    ax.set_xticklabels(listOfSenders)
-    ax.legend()
-
-    def autolabel(rects, xpos='center'):
-        """
-        Attach a text label above each bar in *rects*, displaying its height.
-
-        *xpos* indicates which side to place the text w.r.t. the center of
-        the bar. It can be one of the following {'center', 'right', 'left'}.
-        """
-        xpos = xpos.lower()  # normalize the case of the parameter
-        ha = {'center': 'center', 'right': 'left', 'left': 'right'}
-        offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
-
-        for rect in rects:
-            height = rect.get_height()
-            ax.text(rect.get_x() + rect.get_width()*offset[xpos], 1.01*height,
-                    '{}'.format(height), ha=ha[xpos], va='bottom')
-
-    autolabel(rects1, "left")
-    autolabel(rects2, "right")
-    plt.show()
-
 if __name__ == '__main__':
 
     filename = "F:\Downloads\_chat.txt"
@@ -122,11 +55,22 @@ if __name__ == '__main__':
 
     print("All in all number of messages: {0}".format(len(msgList.msgList)))
 
-    plotTotalNumberOfMessages(msgList)
+    mostCommonWords = statPlot.wordCount(msgList)
+    statPlot.plotMostUsed(mostCommonWords, 'used words', yLabel='Word')
+
+    mostCommonEmojis = statPlot.emojiCount(msgList)
+    statPlot.plotMostUsed(mostCommonEmojis, 'used Emojis', yLabel='Emoji')
+
+    messageByUserCount = statPlot.messageByUserCount(msgList, isMedia=False)
+    mediaByUserCount = statPlot.messageByUserCount(msgList, isMedia=True)
+    statPlot.plotMostUsed(messageByUserCount, 'messages spammers', yLabel='Person')
+    statPlot.plotMostUsed(mediaByUserCount, 'media spammers', yLabel='Person')
 
     mostQuacky = max(msgList.msgList, key=lambda x: len(x.content))
     print("{} {} {} characters".format(mostQuacky.sender,
           mostQuacky.date, len(mostQuacky.content)))
+
+    plt.show()
 
 
 
